c487aad2f929a682ad6eba196093252696bd8510
Anastasyawaruwu <anastasya.e2042@student.ukrimuniversity.ac.id>
2023-05-26 06:36:47 +0700

    Task Pertemuan 18

---
+++ function.php
@@ -0,0 +1,57 @@
+<?php
+session_start();
+// koneksi ke database
+
+$conn = mysqli_connect("localhost","root","","arkatama_store");
+$conn = mysqli_connect("localhost", "root", "", "arkatama_store");
+
+// Add Categories
+
+if(isset($_POST['add-categories'])){
+    $namabarang = $_POST['namabarang'];
+    $tanggal = date("Y-m-d H:i:s");
+
+    $addtotable = mysqli_query($conn,"insert into categories(name,created_at,updated_at) 
+    values ('$namabarang','$tanggal','$tanggal')");
+
+    if($addtotable){
+    $addtotable = mysqli_query($conn, "insert into categories(name) values ('$namabarang')");
+
+    if ($addtotable) {
+        header('location:categories.php');
+    } else {
+        echo "Gagal";
+        header('location:categories.php');
+    }
+}
+}
+ 
+
+// Add Products
+if(isset($_POST['add-products'])){
+    $categoryId = $_POST['categoryId'];
+    $nameproduct = $_POST['nameproduct'];
+    $description = $_POST['description'];
+    $price = $_POST['price'];
+    $status = $_POST['status'];
+    $createdby = $_POST['createdBy'];
+    $verifiedby = $_POST['verifiedBy'];
+    $tanggal = date("Y-m-d H:i:s");
+
+    $addproduct = mysqli_query($conn,"insert into products (category_id,name,description,price,status,created_at,updated_at,created_by,verified_at,verified_by) values ('$categoryId','$nameproduct','$description','$price','$status','$tanggal','$tanggal','$createdby','$tanggal','$verifiedby')");
+
+    if($addproduct){
+        // echo '<script type="text/javascript">
+        // alert ('data berhasil diinput');
+        // </script>';
+    $addproduct = mysqli_query($conn, "insert into products (category_id,name,description,price,status,created_by,verified_at,verified_by) values ('$categoryId','$nameproduct','$description','$price','$status','$createdby','$verifiedby')");
+
+    if ($addproduct) {
+        header('location:products.php');
+        }
+     } else {
+        echo "Gagal";
+        header('location:products.php');
+    }
+}
+?>
